a2d3f3ba7f19535d37a5278a3cb3f719b640df71
Lee Michiko Clone <shileshilehamnida@gmail.com>
2026-04-16 12:39:32 +0800

    Add chart background

--- viz.js
+++ viz.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById("chart");
 const ctx = canvas.getContext("2d");
 
+ctx.fillStyle = "lightgray";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
 // Simple bar chart
 data.forEach((value, index) => {
   ctx.fillStyle = "green";
