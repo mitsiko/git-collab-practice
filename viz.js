cc76b38049334d1533f0ea140fa2a829d0bcc9dc
Lee Michiko Clone <shileshilehamnida@gmail.com>
2026-04-16 12:39:31 +0800

    Change bar color to green

--- viz.js
+++ viz.js
@@ -3,6 +3,6 @@ const ctx = canvas.getContext("2d");
 
 // Simple bar chart
 data.forEach((value, index) => {
-  ctx.fillStyle = "blue";
+  ctx.fillStyle = "green";
   ctx.fillRect(index * 50, 200 - value * 3, 30, value * 3);
 });
