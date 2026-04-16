4e03e79c2ece675611fada5d9e8d21b091c3c117
Lee Michiko Clone <shileshilehamnida@gmail.com>
2026-04-16 12:39:32 +0800

    Improve bar spacing

--- viz.js
+++ viz.js
@@ -4,5 +4,5 @@ const ctx = canvas.getContext("2d");
 // Simple bar chart
 data.forEach((value, index) => {
   ctx.fillStyle = "green";
-  ctx.fillRect(index * 50, 200 - value * 3, 30, value * 3);
+  ctx.fillRect(index * 60, 200 - value * 3, 40, value * 3);
 });
